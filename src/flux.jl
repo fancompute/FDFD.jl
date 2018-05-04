faae18d8b51ebec66037859bd041a801e65a714d
Ian Williamson <iwill@stanford.edu>
2018-05-03 23:42:03 -0700

    Temporary hack to make the flux calculation faster

--- src/flux.jl
+++ src/flux.jl
@@ -30,6 +30,13 @@ function scattering_parameters(fields::Array{<:Field}, d::AbstractDevice)
     return S
 end
 
+"    poynting_hacked(Ez_x, Ez_y, Hx, Hy)"
+function poynting_hacked(Ez_x, Ez_y, Hx, Hy)
+    Sx = -0.5*real.(Ez_x.*conj(Hy));
+    Sy =  0.5*real.(Ez_y.*conj(Hx));
+    return (Sx, Sy)
+end
+
 "    poynting(field::Field)"
 function poynting(field::Field)
     if isa(field, FieldTM)
@@ -54,13 +61,35 @@ function flux_surface(fields::Array{<:Field}, ptmid::AbstractArray{<:Real}, widt
     Nfields = length(fields);
     P = zeros(Float, Nfields);
     for i in eachindex(fields)
-        P[i] = flux_surface(poynting(fields[i]), ptmid, width, nrm);
+        P[i] = flux_surface(fields[i], ptmid, width, nrm);
     end
     return P
 end
 
-"   flux_surface(field::Field, ptmid::AbstractArray{<:Real}, width::Real, nrm::Direction) = flux_surface(poynting(field), ptmid, width, nrm)"
-flux_surface(field::Field, ptmid::AbstractArray{<:Real}, width::Real, nrm::Direction) = flux_surface(poynting(field), ptmid, width, nrm);
+"    flux_surface(field::Field, ptmid::AbstractArray{<:Real}, width::Real, nrm::Direction)"
+function flux_surface(field::Field, ptmid::AbstractArray{<:Real}, width::Real, nrm::Direction)
+    (x0, y0) = coord2ind(field.grid, ptmid);
+    if nrm == DirectionX
+        indx = x0-1:1:x0+1;
+        if isinf(width)
+            indy = 1:field.grid.N[2];
+        else
+            y1 = y2ind(field.grid,ptmid[2]-width/2);
+            y2 = y2ind(field.grid,ptmid[2]+width/2);
+            indy = y1-1:1:y2+1;
+        end
+
+        Ez = view(field.Ez, indx, indy);
+        Ez_x = grid_average(Ez, DirectionX)
+        Ez_y = grid_average(Ez, DirectionY)
+        Hx = view(field.Hx, indx, indy);
+        Hy = view(field.Hy, indx, indy);
+        (Sx, Sy) = poynting_hacked(Ez_x, Ez_y, Hx, Hy)
+        return sum(Sx[2,2:end-1])*dy(field.grid)
+    elseif nrm == DirectionY
+        error("Not implemented yet...")
+    end
+end
 
 "    flux_surface(flux::Flux2D, ptmid::AbstractArray{<:Real}, width::Real, nrm::Direction)"
 function flux_surface(flux::Flux2D, ptmid::AbstractArray{<:Real}, width::Real, nrm::Direction)

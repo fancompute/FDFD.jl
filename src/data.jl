d7809d7b1d5ed610d4f95b1f26bdc107b89fc587
Ian Williamson <iwill@stanford.edu>
2018-05-14 22:17:39 -0700

    Field structs not mutable

--- src/data.jl
+++ src/data.jl
@@ -3,13 +3,13 @@ export SolvedResult, Field, FieldTM, FieldTE, Flux2D, FieldSlice
 abstract type SolvedResult end
 abstract type Field <: SolvedResult end
 
-mutable struct Flux2D <: SolvedResult
+struct Flux2D <: SolvedResult
     grid::Grid{2}
     Sx::Array{Float,2}
     Sy::Array{Float,2}
 end
 
-mutable struct FieldTM <: Field
+struct FieldTM <: Field
     grid::Grid{2}
     Ez::Array{Complex,2}
     Hx::Array{Complex,2}
@@ -21,14 +21,14 @@ function FieldTM(grid::Grid{2}, Ez::Array{<:Complex,1}, Hx::Array{<:Complex,1}, 
 	return FieldTM(grid, reshape(Ez, sz), reshape(Hx, sz), reshape(Hy, sz))
 end
 
-mutable struct FieldTE <: Field
+struct FieldTE <: Field
     grid::Grid{2}
     Hz::Array{Complex,2}
     Ex::Array{Complex,2}
     Ey::Array{Complex,2}
 end
 
-mutable struct FieldSlice <: Field
+struct FieldSlice <: Field
     grid::Grid{1}
     val::Array{Complex,1}
 end
